08f52c8a807ed33b0500665089f4b420f131692f
Allal Mohamed Lamine <allalmohmaedlamine@gmail.com>
2021-07-14 00:01:33 +0100

    update: adding state

--- src/PromiseFiber/index.ts
+++ src/PromiseFiber/index.ts
@@ -1,16 +1,37 @@
-export class PromiseFiber<ResolveT = any, ErrorT = any> {
+export enum EState {
+    Pending = 0,
+    Resolved = 1,
+    Rejected = 2
+}
+
+export class PromiseFiber<ResolveT = any, RejectT = any> {
     public promise: Promise<ResolveT>;
-    public resolve: (data?: ResolveT) => void;
-    public reject: (data?: ResolveT) => void;
+    private _resolve: (data?: ResolveT) => void;
+    private _reject: (data?: RejectT) => void;
+    private _state: EState = EState.Pending;
 
     constructor() {
         this.promise = new Promise((resolve, reject) => {
-            this.resolve = resolve;
-            this.reject = reject;
+            this._resolve = resolve;
+            this._reject = reject;
         });
     }
 
     public await(): Promise<ResolveT> {
         return this.promise;
+    }
+
+    public resolve(data?: ResolveT): void {
+        this._state = EState.Resolved;
+        this._resolve(data);
+    }
+    
+    public reject(data?: RejectT): void {
+        this._state = EState.Rejected;
+        this._reject(data);
+    }
+
+    public getState(): EState {
+        return this._state;
     }
 }
